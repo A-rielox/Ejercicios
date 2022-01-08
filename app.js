a9d493006d23214c04bef06daca01ad05bf1e680
Ariel <arielox.ag@gmail.com>
2022-01-07 21:53:50 -0300

    2022-01-07-21:53

--- app.js
+++ app.js
@@ -21,56 +21,24 @@
 
 // Note
 // consecutive strings : follow one after another without an interruption
-
-// function longestConsec(strarr, k) {
-//    // your code
-//    let newStrarr = [];
-
-//    newStrarr = strarr
-//       .map((item, i) => {
-//          if (i < strarr.length - 1) {
-//             return item + strarr[i + 1];
-//          }
-//       })
-
-//       .sort((a, b) => b.length - a.length);
-
-//    newStrarr.splice(1, strarr.length);
-//    console.log(newStrarr);
-
-//    return newStrarr;
-// }
-
-// console.log(
-//    longestConsec(['tree', 'foling', 'trashy', 'blue', 'abcdef', 'uvwxyz'], 2)
-// );
-
-// arr = ['a', 'b', 'c', 'd', 'e'];
-// // con dos argumentos (1,2) => a partir del elemento 1 borra dos elementos
-// arr.splice(1, 2);
-// console.log(arr);
-//////////////////////////////////////////////////////
-// strarr.length = 0 || k > strarr.length || k <= 0
-
-function longestConsec2(strarr, k) {
+/*
+function longestConsec(strarr, k) {
    let newStrarr = [];
    let palabrota = '';
 
    if (strarr.length === 0 || k > strarr.length || k <= 0) {
       return '';
    } else {
-      for (let i = 0; i < strarr.length - (strarr.length - k); i++) {
+      for (let i = 0; i < strarr.length - (k - 1); i++) {
          const element1 = strarr[i];
          palabrota += element1;
 
          for (let j = 0; j < k - 1; j++) {
             palabrota += strarr[i + 1 + j];
          }
-         // palabrota.splice(strarr.length - k)
-         console.log(palabrota);
+
          newStrarr = [...newStrarr, palabrota];
          palabrota = '';
-         console.log(newStrarr);
       }
 
       newStrarr.sort((a, b) => b.length - a.length);
@@ -78,6 +46,66 @@ function longestConsec2(strarr, k) {
    }
 }
 
+console.log(longestConsec(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 3));
 console.log(
-   longestConsec2(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 3)
+   longestConsec(
+      [
+         'ejjjjmmtthh',
+         'zxxuueeg',
+         'aanlljrrrxx',
+         'dqqqaaabbb',
+         'oocccffuucccjjjkkkjyyyeehh',
+      ],
+      1
+   )
 );
+///////////////////////////////////////////
+function longestConsec2(strarr, k) {
+   let newStrarr = [];
+   // let palabrota = '';
+
+   if (strarr.length === 0 || k > strarr.length || k <= 0) {
+      return '';
+   } else {
+      newStrarr = strarr.map((_, i) => {
+         return strarr.slice(i, i + k).join('');
+      });
+
+      console.log(newStrarr);
+      newStrarr = newStrarr.reduce((tot, curr) => {
+         return curr.length > tot.length ? curr : tot;
+      });
+      return newStrarr;
+   }
+}
+
+console.log(
+   longestConsec2(['zone', 'abigail', 'theta', 'form', 'libe', 'zas'], 2)
+);
+
+// console.log(
+//    longestConsec2(['it', 'wkppv', 'ixoyx', '3452', 'zzzzzzzzzzzz'], 3)
+// );
+// console.log(
+//    longestConsec2(
+//       [
+//          'ejjjjmmtthh',
+//          'zxxuueeg',
+//          'aanlljrrrxx',
+//          'dqqqaaabbb',
+//          'oocccffuucccjjjkkkjyyyeehh',
+//       ],
+//       1
+//    )
+// );
+*/
+/* 
+
+
+
+
+
+
+
+
+*/
