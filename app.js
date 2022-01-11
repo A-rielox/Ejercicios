e1b7ada122ed750cd7e87b12a98e935c387301ee
Ariel <arielox.ag@gmail.com>
2022-01-11 00:55:24 -0300

    2022-01-11-00:55

--- app.js
+++ app.js
@@ -1,64 +1,32 @@
-//66
-// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+// 74
 
-// domainName("http://github.com/carbonfive/raygun") == "github"
-// domainName("http://www.zombie-bites.com") == "zombie-bites"
-// domainName("https://www.cnet.com") == "cnet"
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
-// describe("Sample test", () => {
-//    it("Should pass sample tests", () => {
-//      assert.equal(domainName("http://google.com"), "google");
-//      assert.equal(domainName("http://google.co.jp"), "google");
-//      assert.equal(domainName("www.xakep.ru"), "xakep");
-//      assert.equal(domainName("https://youtube.com"), "youtube");
-//    })
-//  })
+// It should remove all values from list a, which are present in list b keeping their order.
 
-const find = url => {
-   let short = '';
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
 
-   if (!url.startsWith('h') && !url.startsWith('w')) {
-      const indexFDot = url.indexOf('.');
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
-      short = url.slice(0, indexFDot);
-   }
-
-   if (url.startsWith('h')) {
-      const indexSlash = url.indexOf('/');
-      if (url[indexSlash + 2] === 'w') {
-         console.log(indexSlash + 2);
-         const indexFDot = url.indexOf('.');
-         const indexSDot = url.indexOf('.', indexFDot + 1);
-         console.log(indexFDot, indexSDot);
-         short = url.slice(indexFDot + 1, indexSDot);
-         console.log(short);
-      } else {
-         const indexDot = url.indexOf('.');
-         console.log(indexDot);
-
-         short = url.slice(indexSlash + 2, indexDot);
-      }
-   } else if (url.startsWith('w')) {
-      const indexFDot = url.indexOf('.');
-      const indexSDot = url.indexOf('.', indexFDot + 1);
-      console.log(indexFDot, indexSDot);
-
-      short = url.slice(indexFDot + 1, indexSDot);
-   }
-   return short;
+const arrayDiff = (a, b) => {
+   return a.filter(item => !b.includes(item));
 };
-//74
 
-// function domainName(url){
-//    url = url.replace("https://", '');
-//    url = url.replace("http://", '');
-//    url = url.replace("www.", '');
-//    return url.split('.')[0];
-//  };
-//  http://www.p3v4yble3801zwyotp83oxq4e.io
-console.log(find('http://github.com/carbonfive/raygun'));
-console.log(find('http://google.com'));
-console.log(find('https://youtube.com'));
-console.log(find('www.xakep.ru'));
-console.log(find('http://www.p3v4yble3801zwyotp83oxq4e.io'));
-console.log(find('taon7oq1xh.co.za'));
+console.log(arrayDiff([1, 2], [1]));
+console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+console.log(arrayDiff([], [4, 5]));
+console.log(arrayDiff([1, 8, 2], []));
+console.log(arrayDiff([1, 2, 3], [1, 2]));
+//
+//
+//
+//
+//
+//
+//
+//82
+
+// function array_diff(a, b) {
+//    return a.filter(e => !b.includes(e));
+// }
--- listos.js
+++ listos.js
@@ -579,4 +579,47 @@ console.log(find('http://www.p3v4yble3801zwyotp83oxq4e.io'));
 console.log(find('taon7oq1xh.co.za'));
 //💫💫💫💫
 //💫💫💫💫
-//74
+//
+// 74
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+const arrayDiff = (a, b) => {
+   let dif = [];
+
+   if (b.length === 0) {
+      return a;
+   } else {
+      for (let i = 0; i < b.length; i++) {
+         dif = a.filter(item => {
+            console.log(item, b[i]);
+            return item != b[i];
+         });
+         a = dif;
+         console.log(dif);
+      }
+      console.log(dif);
+      return dif;
+   }
+};
+
+// mas chido
+// const arrayDiff = (a, b) => {
+//    return a.filter(item => !b.includes(item));
+// };
+
+console.log(arrayDiff([1, 2], [1]));
+console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+console.log(arrayDiff([], [4, 5]));
+console.log(arrayDiff([1, 8, 2], []));
+console.log(arrayDiff([1, 2, 3], [1, 2]));
+//82
+//💫💫💫💫
+//💫💫💫💫
