6927aaf636d48132d58e0dfe941fcd73fc25dda2
Ariel <arielox.ag@gmail.com>
2022-01-14 02:48:20 -0300

    2022-01-14-02:48

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // 118
+// 5 kyu - Regex Password Validation
 // You need to write regex that will validate a password to make sure it meets the following criteria:
 
 // At least six characters long
@@ -20,10 +21,17 @@
 // }
 
 function validate(password) {
-   return /[a-zA-Z0-9]/.test(password);
+   return /(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])\w{6,}/.test(password);
 }
 
+console.log(validate('djI38D55'));
+console.log(validate('a2.d412'));
+console.log(validate('JHD5FJ53'));
+console.log(validate('!fdjn345'));
+console.log(validate('Password123'));
+console.log(validate('djI38D55'));
 console.log(validate('djI38D55'));
 
 // \w -> alfanumericos
 // \w{6} -> alfanumericos de 6 o mas
+// [a-zA-Z0-9] -> letras minusculas, mayusculas y nums
