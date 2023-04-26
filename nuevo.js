61c6255d7b219199eab77cda54db23babf175f08
A-rielox <arielox.ag@gmail.com>
2023-04-26 08:52:43 -0600

    me falto el comment

--- nuevo.js
+++ nuevo.js
@@ -1,3 +1,58 @@
+// prettier-ignore
+const roma = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 };
+function hacerRomano(num) {
+   let romNum = '';
+   let rest = num;
+
+   for (const key in roma) {
+      while (rest >= roma[key]) {
+         rest = rest - roma[key];
+
+         romNum += key;
+      }
+   }
+   return romNum;
+}
+
+console.log(hacerRomano(19));
+console.log(hacerRomano(21));
+console.log(hacerRomano(2000));
+/////////////////////////////////
+
+// 6 kyu   Does my number look big in this?
+// A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+// For example, take 153 (3 digits), which is narcissistic:
+
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1652 (4 digits), which isn't:
+
+//     1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+// The Challenge:
+
+// Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+
+// This may be True and False in your language, e.g. PHP.
+
+// Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+// function narcissistic(value) {
+//    const digits = [...value.toString()].map(i => +i);
+
+//    let narciSum = digits.reduce((t, i) => {
+//       t = t + Math.pow(i, digits.length);
+
+//       return t;
+//    }, 0);
+
+//    return value === narciSum ? true : false;
+// }
+
+// console.log(narcissistic(7));
+// console.log(narcissistic(153));
+// console.log(narcissistic(1652));
+// console.log(narcissistic(371));
+
 /////////////////////////////////////////////////////
 
 // 6 kyu   Your order, please
--- regex.js
+++ regex.js
@@ -1,43 +1,188 @@
-// 118
-// 5 kyu - Regex Password Validation
-// You need to write regex that will validate a password to make sure it meets the following criteria:
+let yo = 'Arielox';
 
-// At least six characters long
-// contains a lowercase letter
-// contains an uppercase letter
-// contains a number
-// Valid passwords will only be alphanumeric characters.
+let yoArr = [...yo];
 
-// ('djI38D55'), 'djI38D55 - true'
-// ('a2.d412'), 'a2.d412 - false'
-// ('JHD5FJ53'), 'JHD5FJ53 - false'
-// ('!fdjn345'), '!fdjn345 - false'
-// ('jfkdfj3j'), 'jfkdfj3j - false'
-// ('123'), '123 - false'
-// ('abc'), 'abc - false'
-// ('Password123'), 'Password123 - true'
-// function validate(password) {
-//    return /(put answer here)/.test(password);
-// }
+console.log(yo);
+console.log(...yo);
+console.log(yoArr);
 
-function validate(password) {
-   return /(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])\w{6,}/.test(password);
+for (const el of yoArr.entries()) {
+   console.log(el);
 }
 
-console.log(validate('djI38D55'));
-console.log(validate('a2.d412'));
-console.log(validate('JHD5FJ53'));
-console.log(validate('!fdjn345'));
-console.log(validate('Password123'));
-console.log(validate('djI38D55'));
-console.log(validate('djI38D55'));
+// prettier-ignore
+const roma = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 };
 
-// \w -> alfanumericos
-// \w{6} -> alfanumericos de 6 o mas
-// [a-zA-Z0-9] -> letras minusculas, mayusculas y nums
-// ^               # start of input
-// (?=.*?[A-Z])    # Lookahead to make sure there is at least one upper case letter
-// (?=.*?[a-z])    # Lookahead to make sure there is at least one lower case letter
-// (?=.*?[0-9])    # Lookahead to make sure there is at least one number
-// [A-Za-z0-9]{6,} # Make sure there are at least 6 characters of [A-Za-z0-9]
-// $               # end of input
+console.log(Object.keys(roma));
+console.log(Object.values(roma));
+console.log(...Object.entries(roma));
+
+for (const key in roma) {
+   console.log(key);
+}
+
+let yoyo = 'ariel godoy palominos';
+console.log(new Set(yoyo));
+console.log(new Set(yoyo).size);
+console.log(...new Set(yoyo));
+console.log([...new Set(yoyo)]);
+
+console.log(yoyo.toUpperCase());
+
+let singleName = yoyo.split(' ');
+let cap = singleName.map(name => name[0].toUpperCase() + name.slice(1));
+
+console.log(singleName);
+console.log(cap.join(' '));
+
+/////
+console.log(yoyo.replaceAll('o', '0').replaceAll('a', '@').replaceAll('i', 1));
+
+/////
+const creditCard = '123456789';
+
+const mask = cc => {
+   let last = cc.slice(-4);
+   let padded = last.padStart(cc.length, '#');
+
+   return padded;
+};
+
+console.log(mask(creditCard).repeat(2));
+/////
+console.log([...yo].reverse().join(''));
+console.log(Array.from(yo).reverse().join(''));
+console.log([...yoyo].indexOf('o'));
+console.log([...yoyo].indexOf('o', 7 + 1));
+
+/////
+const nums = [3, 5, 7, 12, 42, 633, 235, 22, 999, 55, 11];
+
+const filtro = item => {
+   return item > 12;
+};
+
+let mayores = nums.filter(filtro);
+
+console.log(mayores);
+/////
+const numsForPrime = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+const filtroPrime = item => {
+   for (let i = 2; item > i; i++) {
+      if (item % i === 0) {
+         console.log(item);
+
+         return false;
+      }
+   }
+   console.log(item);
+   return item > 1;
+};
+
+let justThePrimes = numsForPrime.filter(filtroPrime);
+
+console.log(justThePrimes);
+
+////////////////////
+const numsMax = nums.reduce((tot, curr) => {
+   if (curr > tot) {
+      tot = curr;
+   }
+
+   return tot;
+}, 0);
+
+console.log(numsMax);
+
+////////////////////
+// primero mayor q 10
+
+const filterBiger = item => {
+   return item > 10;
+};
+
+const mayorQueDiez = numsForPrime.findIndex(filterBiger);
+console.log(mayorQueDiez);
+console.log(numsForPrime[mayorQueDiez]);
+
+////////////////////
+// todos los mayores que 10
+
+let forBiger = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1, 2, 3, 14, 15];
+
+let arrMayoresQ10 = [];
+
+for (let i = 0; i < forBiger.length; i++) {
+   if (forBiger[i] > 10) arrMayoresQ10.push(forBiger[i]);
+}
+
+console.log(arrMayoresQ10);
+
+//////////////////////////
+const alphabetPosition = text => {
+   // prettier-ignore
+   const letters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+
+   let lett = [...text].filter(
+      char => char.toUpperCase() !== char.toLowerCase()
+   );
+
+   let indexes = lett.map(char => {
+      return letters.indexOf(char.toUpperCase()) + 1;
+   });
+
+   return indexes.join();
+};
+
+console.log(alphabetPosition('hola hola que hace.'));
+
+//////////////////////////
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+   let singleW = words.split(' ');
+   let newArrOfW = [...singleW];
+
+   singleW.forEach(el => {
+      let index = +el
+         .split('')
+         .filter(char => char.toLowerCase() === char.toUpperCase());
+
+      newArrOfW.splice(index - 1, 1, el);
+   });
+
+   console.log(singleW);
+   console.log(newArrOfW);
+
+   return newArrOfW.join(' ');
+}
+
+console.log(order('is2 Thi1s T4est 3a'));
+// console.log(order('4of Fo1r pe6ople g3ood th5e the2'));
+// console.log(order(""));
+
+//////////////////////////
+// prettier-ignore
+const romaObj = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 };
+
+function toRom(num) {
+   let rom = '';
+   let rest = num;
+
+   for (const key in romaObj) {
+      while (rest >= romaObj[key]) {
+         rom += key;
+         rest -= romaObj[key];
+      }
+   }
+
+   return rom;
+}
+
+console.log(toRom(19));
+console.log(toRom(21));
+console.log(toRom(2000));
